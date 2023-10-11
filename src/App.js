4a4ca2cc25754307f4bf56265b55c85855813968
GMDLULI <113610455+GMDLULI@users.noreply.github.com>
2023-10-11 12:22:00 +0200

    react weather app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Weather from "./Weather.js"
+function App() {
+  return (
+    <div className="App">
+      <div className='contanier'>
+        <Weather/>
+      <p> This project is coded by <a href="#" alt="">Gugulethu Mdlui</a>and is  <a href='#' alt="" >Open sourced on Github</a></p>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
